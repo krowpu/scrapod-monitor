5701bba6bf5140008cc844c766e5b7446f619b3c
krowpu <krowpu@tightmail.com>
2017-03-20 04:42:14 +0500

    Display sessions

--- lib/scrapod/monitor.rb
+++ lib/scrapod/monitor.rb
@@ -11,11 +11,20 @@ module Scrapod
     set :root, File.expand_path('../..', File.dirname(__FILE__))
 
     get '/' do
-      'Hello, World!'
+      erb :index, locals: { sessions: sessions }
     end
 
     def redis
       @redis ||= Redis.new
+    end
+
+    def sessions
+      redis.smembers('sessions').map do |session_id|
+        {
+          id: session_id,
+          started_at: redis.get("session:#{session_id}:started_at"),
+        }
+      end
     end
   end
 end
