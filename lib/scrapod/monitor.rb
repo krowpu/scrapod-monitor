6a6095e535cc46773b6e911edf0320eca02fa90a
krowpu <krowpu@tightmail.com>
2017-03-23 03:17:40 +0500

    Use Scrapod::Redis models

--- lib/scrapod/monitor.rb
+++ lib/scrapod/monitor.rb
@@ -3,6 +3,8 @@
 require 'sinatra/base'
 require 'redis'
 
+require 'scrapod/redis'
+
 module Scrapod
   ##
   # Scrapod monitoring dashboard.
@@ -11,20 +13,11 @@ module Scrapod
     set :root, File.expand_path('../..', File.dirname(__FILE__))
 
     get '/' do
-      erb :index, locals: { sessions: sessions }
+      erb :index, locals: { sessions: Redis::Session.all(redis) }
     end
 
     def redis
-      @redis ||= Redis.new
-    end
-
-    def sessions
-      redis.smembers('sessions').map do |session_id|
-        {
-          id: session_id,
-          started_at: redis.get("session:#{session_id}:started_at"),
-        }
-      end
+      @redis ||= ::Redis.new
     end
   end
 end
